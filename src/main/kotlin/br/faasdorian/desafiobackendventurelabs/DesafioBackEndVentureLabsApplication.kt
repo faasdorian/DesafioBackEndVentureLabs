ca10a833265efcafa04bd41f51bff045bcd9a2f6
faasdorian <faasdorian@gmail.com>
2021-12-16 20:07:45 -0300

    Criação do projeto.

---
+++ src/main/kotlin/br/faasdorian/desafiobackendventurelabs/DesafioBackEndVentureLabsApplication.kt
@@ -0,0 +1,11 @@
+package br.faasdorian.desafiobackendventurelabs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesafioBackEndVentureLabsApplication
+
+fun main(args: Array<String>) {
+    runApplication<DesafioBackEndVentureLabsApplication>(*args)
+}
---
+++ src/test/kotlin/br/faasdorian/desafiobackendventurelabs/DesafioBackEndVentureLabsApplicationTests.kt
@@ -0,0 +1,15 @@
+package br.faasdorian.desafiobackendventurelabs
+
+import br.faasdorian.desafiobackendventurelabs.model.Companhia
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesafioBackEndVentureLabsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
